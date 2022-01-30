b232457612a1e1314e173accb99e1f94eea2938f
Emmanuel Garcia <blasten@gmail.com>
2022-01-29 16:50:25 -0800

    complete example

--- example/main.go
+++ example/main.go
@@ -2,16 +2,71 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// Running the example:
+//   Pull files: go run main.go pull --dir=<local-directory> --host=:1234
+//   Push files: go run main.go push --dir=<local-directory> --host=:1234
+
 package main
 
 import (
-	"fmt"
+	"flag"
+	"log"
+	"net"
+	"os"
 
 	"github.com/blasten/rsync"
 )
 
 func main() {
-	rsync.Push(nil, "src")
-	rsync.Pull(nil, "src")
-	fmt.Println("Run")
+	subCmd := flag.NewFlagSet("subcommand", flag.ExitOnError)
+
+	var host string
+	subCmd.StringVar(&host, "host", ":1234", "Specify the hostname. e.g. --host=server.io:1234")
+
+	var dir string
+	subCmd.StringVar(&dir, "dir", "", "This is the directory to push files to or pull files from. e.g. --dir=~/somepath/")
+
+	if len(os.Args) < 2 {
+		panic("Use go run main.go push <flags> or go run main.go pull <flags>")
+	}
+
+	subCmd.Parse(os.Args[2:])
+
+	if len(dir) == 0 {
+		panic("Specify a directory using the --dir flag")
+	}
+
+	op := os.Args[1]
+	switch op {
+	case "push":
+		conn, err := net.Dial("tcp", host)
+		if err != nil {
+			log.Fatalf("could not push: %s", err.Error())
+		}
+		defer conn.Close()
+
+		err = rsync.Push(conn, dir)
+		if err != nil {
+			log.Fatalf("could not push: %s", err.Error())
+		}
+	case "pull":
+		l, err := net.Listen("tcp", host)
+		if err != nil {
+			log.Fatalf("could not listen: %s", err.Error())
+		}
+		defer l.Close()
+
+		for {
+			conn, err := l.Accept()
+			if err != nil {
+				log.Fatalf("coult not accept connection: %s", err.Error())
+			}
+			err = rsync.Pull(conn, dir)
+			if err != nil {
+				log.Fatalf("could not pull: %s", err.Error())
+			}
+		}
+	default:
+		panic("use push or pull")
+	}
 }
